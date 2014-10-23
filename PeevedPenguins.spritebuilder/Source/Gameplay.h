5a1e10746440ee66143616ac38a97ee7c7417634
aoraptor <aowongster@gmail.com>
2014-10-22 21:30:15 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,16 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Alistair on 10/22/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Gameplay : CCNode {
+    
+}
+
+@end
