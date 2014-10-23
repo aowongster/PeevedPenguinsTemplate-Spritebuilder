058289001f77a7972c4448824b632f462b1ec51f
aoraptor <aowongster@gmail.com>
2014-10-22 19:56:16 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,16 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Alistair on 10/22/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Seal : CCSprite {
+    
+}
+
+@end
